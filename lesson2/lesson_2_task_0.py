8a9f076651d55081f81de776a2b8741afd596c1b
Kolpina Margarita <margarita@oseung-ug-ui-MacBookAir.local>
2024-05-19 21:26:54 +0900

    add second homework

---
+++ lesson2/lesson_2_task_0.py
@@ -0,0 +1,6 @@
+my_age_as_string = input("What is your age?")
+
+print("Your age: " + my_age_as_string)
+my_age = int(my_age_as_string)
+my_age = my_age + 1
+print("Your age in one year will be" , my_age)
